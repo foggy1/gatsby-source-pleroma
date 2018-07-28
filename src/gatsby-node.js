66df3c1aa6541761d6d137564c47fdaa9011de88
Austin Lanari <austin.lanari@gmail.com>
2018-07-28 09:15:56 -0700

    placeholder attachments for when none are in chunk

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -15,9 +15,11 @@ exports.sourceNodes = async (
   // URL to the pleroma instance (no trailing slash) and your userId are **required**
   const { instance, userId } = configOptions
 
-  const count = configOptions.count || 20
 
-  const apiUrl = `${instance}/api/qvitter/statuses/user_timeline.json?user_id=${userId}&count=${count}`
+  // Note: as of 7/28/18, the `count` param doesn't actually do anything: it's ALWAYS 20.
+  //const count = configOptions.count || 20
+
+  const apiUrl = `${instance}/api/qvitter/statuses/user_timeline.json?user_id=${userId}`
 
   const processPost = post => {
     const nodeId = createNodeId(`pleroma-post-${post.id}`)
@@ -27,9 +29,14 @@ exports.sourceNodes = async (
           .update(nodeContent)
           .digest('hex')
 
+    // If there are not any attachments in your dataset, declarative calls to the attachment node will fail
+    // We add a placeholder here with an empty string and explicit empty boolean to make sure it stays in the schema
+    // Note: id is often null on REAL attachments, and is not a reliable field on which to filter empty attachments.
+    const attachmentPlaceholder = { id: null, url: '', empty: true, mimetype: '', oembed: null }
+
     const nodeData = {
       ...post,
-      attachments: [...post.attachments],
+      attachments: post.attachments.length ? post.attachments : [attachmentPlaceholder],
       id: nodeId,
       parent: null,
       children: [],
